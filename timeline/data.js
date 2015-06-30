8cd04bcabda3ce0cea36a4e38ff7ac5a0dc69fe1
SurfaceW <yeqingnan@live.cn>
2015-07-01 02:19:22 +0800

    new timeline for career
    timelineJS thanks : )

---
+++ timeline/data.js
@@ -0,0 +1,154 @@
+
+var timelineData = {
+    "timeline":
+    {
+        "headline":"SurfaceW 前端工程师<br/> 2012 - 2015",
+        "startDate":"2012,12",
+        "text":"<p>这是我作为一名前端工程师的旅程。</p>",
+        "type":"default",
+        "asset":
+        {
+            "media":"image/cover.jpg",
+            "credit":"图片：唯美主义者",
+            "caption":"在设计的道路上，我是一个唯美主义者，我认为发现美并创造美是最高的准则。"
+        },
+        "date": [
+            {
+                "startDate":"2015,5,1",
+                "endDate":"2015,6,30",
+                "headline":"ETIWTT App",
+                "text":"<p>贯穿整个月的项目，尝试挠自己的痒点，设计一款属于自己的应用。并将其雏形放置到了百度的 BAE 上。赞！毕竟是第一个经过部署的 Node 应用。</p>",
+                "asset":
+                {
+                    "media":"image/ETIWTT.png",
+                    "credit":"Apps Capture",
+                    "caption":"其实设计这样的应用还是蛮有意思的 :)"
+                }
+            },
+            {
+                "startDate":"2012,12",
+                "endDate":"2012,12",
+                "headline":"学院官方网站设计比赛",
+                "text":"第一次用心设计的网页作品总是值得留念的。这幅参加计通学院网页设计比赛的作品获得了第一名，也令我开始了前端的旅程。",
+                "asset":
+                {
+                    "media":"image/01.png",
+                    "credit":"Safari 截图",
+                    "caption":"大学一年级新生的作品！"
+                }
+            },
+            {
+                "startDate":"2013,5",
+                "endDate":"2013,9",
+                "headline":"社会实践：Kunotebook.com",
+                "text":"组建社会实践团队，构建一个服务于大众的以普及笔记本知识为目的的公益网站。我们自己动手设计网站，组织网站内容，部署网站。" +
+                "<br/>我们融入了团队协作的精神，最终实现了破1W的访问量。也感受了一个网站的建设过程，学得不少经验！",
+                "asset":
+                {
+                    "media":"image/02.jpeg",
+                    "credit":"浏览器截图",
+                    "caption":"无论怎么样，亲自设计的作品，总是令人欣慰的！"
+                }
+            },
+            {
+                "startDate":"2013,10",
+                "endDate":"2014,6",
+                "headline":"KGraph 与 806创新实验室",
+                "text":"组建KGraph团队，在学校的创新实验室里带领团队进行一个 Web App 的研发,⺫的在于帮助人们管理自己的知识分支。" +
+                "这是我第⼀次带领⼀个小团队来实现我们⾃⼰的想法。我们利用 Web 技术构建了⼀个 SPA(单页) 应⽤,并且利用 " +
+                "CakePHP 实现后端数据的⽀持。整个 WebApp 是基于 Cavnas 实现，效果挺赞！",
+                "asset":
+                {
+                    "media":"image/10.png",
+                    "credit":"登录界面截图",
+                    "caption":"这是我对创新和产品的一次独立尝试！"
+                }
+            },
+            {
+                "startDate":"2013,11",
+                "endDate":"2013,12",
+                "headline":"学校招生网站设计",
+                "text":"参与学校的本科招生网站的原型设计，我就喜欢设计！！！",
+                "asset":
+                {
+                    "media":"image/03.png",
+                    "credit":"界面截图",
+                    "caption":"多少为学校做点什么还是值得的，不过那个时候更喜欢的是这种小活儿赚得的外快！XD"
+                }
+            },
+            {
+                "startDate":"2014,3",
+                "endDate":"2014,5",
+                "headline":"跟着研究生学长跟进一个商用 CRM 系统",
+                "text":"虽然是做苦力，但是收获很多，毕竟是第一次接触商用的站点制作，而且整个程序是纯基于 Restful-API 的单页应用。",
+                "asset":
+                {
+                    "media":"image/11.jpg",
+                    "credit":"界面截图",
+                    "caption":"因为是内部系统，所以在此省略截图 = ="
+                }
+            },
+            {
+                "startDate":"2014,7",
+                "endDate":"2014,10",
+                "headline":"百度公司实习与模板开发平台",
+                "text":"致力于一个提高内部研发效率的模板开发平台的前端研发。"
+                    + "对这种大型系统的研发是比较耗时的，但是很值得，因为对我自身的历练很多。我从中学会了很多新鲜的东西。"
+                    + "当然，在百度，更多的收获是对职场的认知和技术视野的拓展！",
+                "asset":
+                {
+                    "media":"image/04.png",
+                    "credit":"界面截图",
+                    "caption":"这真的算是一个功能繁多的内部平台的。P.S. 串行的Ajax请求都能够发一堆！XD"
+                }
+            },
+            {
+                "startDate":"2014,9",
+                "headline":"个人博客的 Jeklly 实现",
+                "text":'深度定制之后的 Jekyll 真的很棒！加之对 Markdown 的自定义，感觉好多了！<a href="http://blog.surfacew.com">blog.surfacew.com</a>',
+                "asset":
+                {
+                    "media":"image/06.png",
+                    "credit":"界面截图",
+                    "caption":"这真的算是一个功能繁多的内部平台的。P.S. 串行的Ajax请求都能够发一堆！XD"
+                }
+            },
+            {
+                "startDate":"2015,5",
+                "endDate":"2015,6",
+                "headline":"Ruyin 3D 旋转瓶的开发",
+                "text":"帮助朋友用 WebGL 实现了一个 3D 旋转的瓶子，人们能够任意上传贴图贴在瓶身上，感觉很炫酷！这充分肯定了 Web 技术的无限可能！",
+                "asset":
+                {
+                    "media":"image/07.png",
+                    "credit":"界面截图",
+                    "caption":"给 WebGL 和 Three.js 点赞！"
+                }
+            },
+            {
+                "startDate":"2015,3",
+                "endDate":"2015,7",
+                "headline":"Baidu 百小度AI界面优化",
+                "text":"终于能够在 Baidu 的产品线上做文章了，我很喜欢这种模块化的开发，给小度的代码点赞！",
+                "asset":
+                {
+                    "media":"image/08.png",
+                    "credit":"界面截图",
+                    "caption":"话说这个内测版的百小度有时候说话超贱！也真是苦了百度NPL的团队了。"
+                }
+            },
+            {
+                "startDate":"2015,6",
+                "endDate":"2015,7",
+                "headline":"搜索结果模板开发以及优化",
+                "text":"天气模板、油价模板，线上展示，每天几百万的PV，算是我写过的展示得最多的代码了！",
+                "asset":
+                {
+                    "media":"image/09.png",
+                    "credit":"界面截图",
+                    "caption":"线上的产品，最有意思了！"
+                }
+            }
+        ]
+    }
+}

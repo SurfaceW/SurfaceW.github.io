760ee932a19529d8bf7710b732539c77674ef9c6
qingnan.yqn <qingnan.yqn@alibaba-inc.com>
2016-02-24 15:15:13 +0800

    speed test sample

--- js/main.js
+++ js/main.js
@@ -15,3 +15,69 @@
 // }
 
 // window.onload = chooseBG;
+
+/* 
+ * Timing API to monitor Page Performance
+ * 
+ * @Author: qingnan.yqn
+ * @Date: 2016-02-24
+ * @Last Modified by:
+ * @Last Modified time: 
+ */
+
+(function () {
+
+  'use strict'
+
+  var fb = {
+    networkType: null,
+    time: {}
+  };
+
+  // only work on mobile device
+  if (window.WindVane) {
+    WindVane.call('WVNative', 'getCurrentNetStatus', {}, function (e) {
+      fb.networkType = e.netStatus;
+    });
+  }
+  
+  var timing;
+  var startTime = new Date().getTime();
+
+  if (window.performance && window.performance.timing) {
+    timing = window.performance.timing;
+    window.onload = function () {
+      fb.time.responseReady = timing.responseEnd - timing.navigationStart;
+      fb.time.domReady = timing.domComplete - startTime;
+      fb.time.pageReady = new Date().getTime() - startTime;
+    } 
+  } else {
+    fb.time.responseReady = null;
+    document.addEventListener('DOMContentLoaded', function () {
+      fb.time.domReady = new Date().getTime() - startTime;
+    }, false);
+    window.onload = function () {
+      fb.time.pageReady = new Date().getTime() - startTime;
+    }
+  }
+
+  // fb.time.responseReady = null;
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   fb.time.domReady = new Date().getTime() - startTime;
+  // }, false);
+  // window.onload = function () {
+  //   fb.time.pageReady = new Date().getTime() - startTime;
+  // }
+  
+  // call manually while all services are ready
+  window._services_all_ready = function () {
+    fb.time.serviceReady = new Date().getTime() - startTime;
+    console.log(fb.time);
+  }
+
+})();
+
+setTimeout(function () {
+  _services_all_ready()
+}, 300);
+
